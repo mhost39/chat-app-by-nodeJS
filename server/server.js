ed2107a0d83bada260a966f5a07963792623a523
hassan <mhost39@gmail.com>
2018-03-18 16:43:44 +0200

    connect and disconnect to server and clinte

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname , '/../public');
 var port = process.env.PORT || 3000
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server)
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log('one user connected');
 
-app.listen(port,()=>{
+  socket.on('disconnect',()=>{
+    console.log('user disconnected');
+  });
+});
+server.listen(port,()=>{
   console.log(`server is start up on port ${port}`);
 });
